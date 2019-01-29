861b8ed442be392c763e4579c032bf3059b45742
Federico Pugnaloni <federico.pugnaloni@streetlib.com>
2019-01-29 20:43:44 +0100

    Initial Commit

---
+++ MyLocations/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Federico Pugnaloni on 29/01/2019.
+//  Copyright © 2019 Federico Pugnaloni. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyLocations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Federico Pugnaloni on 29/01/2019.
+//  Copyright © 2019 Federico Pugnaloni. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
