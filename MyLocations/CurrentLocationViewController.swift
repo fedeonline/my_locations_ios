3e8a2c9bd228e146447ad82f8ac374cba60f273c
Federico Pugnaloni <federico.pugnaloni@streetlib.com>
2019-01-30 13:16:45 +0100

    Add Tag Location and Category picker.

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -12,9 +12,15 @@ import CoreLocation
 class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate {
     // MARK: properties
     let locationManager = CLLocationManager()
+    let geocoder = CLGeocoder()
     var location: CLLocation?
+    var placemark: CLPlacemark?
     var updatingLocation = false
+    var performingReverseGeocoding = false
     var lastLocationError: Error?
+    var lastGeocodingError: Error?
+    var timer: Timer?
+    
     
     // MARK: - IBOutlet
     @IBOutlet weak var messageLabel: UILabel!
@@ -36,8 +42,16 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             showLocationServiceDeniedAlert()
             return
         }
-        
-        startLocationManager()
+
+        if updatingLocation {
+            stopLocationManager()
+        } else {
+            location = nil
+            lastLocationError = nil
+            placemark = nil
+            lastGeocodingError = nil
+            startLocationManager()
+        }
         updateLabels()
     }
     
@@ -45,6 +59,20 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         updateLabels()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
     }
     
     // MARK: - CLLocationManagerDelegate
@@ -65,9 +93,91 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         let newLocation = locations.last!
         print("didUpdateLocation \(newLocation)")
         
-        location = newLocation
-        lastLocationError = nil
-        updateLabels()
+        // we're not moving so fast
+        if newLocation.timestamp.timeIntervalSinceNow < -5 {
+            return
+        }
+        
+        // invalid measurement
+        if newLocation.horizontalAccuracy < 0 {
+            return
+        }
+        
+        // calculate distance between last and new location
+        var distance = CLLocationDistance(Double.greatestFiniteMagnitude)
+        if let location = location {
+            distance = newLocation.distance(from: location)
+        }
+        
+        // new reading is more useful than the previous one
+        if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
+            lastLocationError = nil
+            location = newLocation
+            
+            if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
+                print("*** We're done!")
+                stopLocationManager()
+                // force reverse geocoding if calculated distance is greater than 0
+                if distance > 0 {
+                    performingReverseGeocoding = false
+                }
+            }
+            
+            if !performingReverseGeocoding {
+                print("*** Going to geocode")
+                
+                performingReverseGeocoding = true
+                
+                geocoder.reverseGeocodeLocation(newLocation, completionHandler: {
+                    placemarks, error in
+                    
+                    self.lastLocationError = error
+                    if error == nil, let p = placemarks, !p.isEmpty {
+                        self.placemark = p.last!
+                    } else {
+                        self.placemark = nil
+                    }
+                    self.performingReverseGeocoding = false
+                    self.updateLabels()
+                })
+            } else if distance < 1 {
+                // coordinate from this reading is not significantly different from the previous
+                let timeInterval = newLocation.timestamp.timeIntervalSince(location!.timestamp)
+                
+                // more than 10 seconds have elapsed since the last readings
+                if timeInterval > 10 {
+                    // it's safe to stop
+                    print("*** Force done!")
+                    stopLocationManager()
+                    updateLabels()
+                }
+            }
+        }
+    }
+    
+    func startLocationManager() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            updatingLocation = true
+            
+            // set a timer
+            timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(didTimeOut), userInfo: nil, repeats: false)
+        }
+    }
+    
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+            
+            // invalidate timer if we found a location before time out
+            if let timer = timer {
+                timer.invalidate()
+            }
+        }
     }
 
     func showLocationServiceDeniedAlert() {
@@ -81,12 +191,47 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
         present(alert, animated: true, completion: nil)
     }
     
+    func string(from placemark: CLPlacemark) -> String {
+        var line1 = ""
+        if let s = placemark.subThoroughfare {
+            line1 += s + " "
+        }
+        // street name
+        if let s = placemark.thoroughfare {
+            line1 += s
+        }
+        
+        var line2 = ""
+        // city
+        if let s = placemark.locality {
+            line2 += s + " "
+        }
+        // state or province
+        if let s = placemark.administrativeArea {
+            line2 += s + " "
+        }
+        if let s = placemark.postalCode {
+            line2 += s
+        }
+        
+        return line1 + "\n" + line2
+    }
+    
     func updateLabels() {
         if let location = location {
             latitudeLabel.text = String(format: "%.8f", location.coordinate.latitude)
             longitudeLabel.text = String(format: "%.8f", location.coordinate.longitude)
             tagButton.isHidden = false
             messageLabel.text = ""
+            if let placemark = placemark {
+                addressLabel.text = string(from: placemark)
+            } else if performingReverseGeocoding {
+                addressLabel.text = "Searching for Address..."
+            } else if lastGeocodingError != nil {
+                addressLabel.text = "error finding Address"
+            } else {
+                addressLabel.text = "No Address Found"
+            }
         } else {
             latitudeLabel.text = ""
             longitudeLabel.text = ""
@@ -108,24 +253,25 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             }
             messageLabel.text = statusMessage
         }
+        configureGetButton()
     }
     
-    func startLocationManager() {
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-            updatingLocation = true
-        }
-    }
-    
-    func stopLocationManager() {
+    func configureGetButton() {
         if updatingLocation {
-            locationManager.stopUpdatingLocation()
-            locationManager.delegate = nil
-            updatingLocation = false
+            getButton.setTitle("Stop", for: .normal)
+        } else {
+            getButton.setTitle("Get My Location", for: .normal)
         }
     }
-
+    
+    // MARK: - Objective-C selectors
+    @objc func didTimeOut() {
+        print("*** Time out")
+        if location == nil {
+            stopLocationManager()
+            lastLocationError = NSError(domain: "MyLocationsErrorDomain", code: 1, userInfo: nil)
+            updateLabels()
+        }
+    }
 }
 
---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,127 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Federico Pugnaloni on 30/01/2019.
+//  Copyright © 2019 Federico Pugnaloni. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
+
+class LocationDetailsViewController: UITableViewController {
+    // MARK: - Properties
+    var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
+    var placemark: CLPlacemark?
+    var categoryName = "No Category"
+    
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    // MARK: - Actions
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func categoryPickerDidPickCategory(_ segue: UIStoryboardSegue) {
+        let controller = segue.source as! CategoryPickerViewController
+        categoryName = controller.selectedCategoryName
+        categoryLabel.text = categoryName
+    }
+    
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        
+        descriptionTextView.text = ""
+        categoryLabel.text = categoryName
+        
+        latitudeLabel.text = String(format: "%.8f", coordinate.latitude)
+        longitudeLabel.text = String(format: "%.8f", coordinate.longitude)
+        
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        
+        dateLabel.text = format(date: Date())
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    // MARK: - Table View delegates
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 88
+        } else if indexPath.section == 2 && indexPath.row == 2 {
+            addressLabel.frame.size = CGSize (width: view.bounds.size.width - 120, height: 10000)
+            addressLabel.sizeToFit()
+            addressLabel.frame.origin.x = view.bounds.size.width - addressLabel.frame.size.width - 16
+            
+            return addressLabel.frame.size.height + 20
+        } else {
+            return 44
+        }
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "PickCategory" {
+            let controller = segue.destination as! CategoryPickerViewController
+            controller.selectedCategoryName = categoryName
+        }
+    }
+    
+    // MARK: - Private Methods
+    func string(from placemark: CLPlacemark) -> String {
+        var text = ""
+        if let s = placemark.subThoroughfare {
+            text += s + " "
+        }
+        // street name
+        if let s = placemark.thoroughfare {
+            text += s + ", "
+        }
+        // city
+        if let s = placemark.locality {
+            text += s + ", "
+        }
+        // state or province
+        if let s = placemark.administrativeArea {
+            text += s + " "
+        }
+        if let s = placemark.postalCode {
+            text += s + ", "
+        }
+        if let s = placemark.country {
+            text += s
+        }
+        
+        return text
+    }
+    
+    func format(date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+}
